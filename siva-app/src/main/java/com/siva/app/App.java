ecbb1d5ddab3c8bcfdb9ae8c9d52bc1ed28a4302
SABI <SABI>
2020-06-08 17:19:04 +0530

    Adding POM.xml file

---
+++ siva-app/src/main/java/com/siva/app/App.java
@@ -0,0 +1,13 @@
+package com.siva.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First Java Maven Build Program!" );
+    }
+}
